8b04ec2a75e282614f2f6c59457ffec5f1c13f46
Casey Jones <cjone183@msudenver.edu>
2019-11-25 22:45:12 -0700

    also remember no answers

--- PrologExpertSystem.pl
+++ PrologExpertSystem.pl
@@ -3,22 +3,30 @@
 
 begin :- write("Welcome to the Beer Selector..."), nl,
          write("an Expert System to pick the beer you need!"), nl,
-         retractall(fact(_)),
+         retractall(yes(_)),
+         retractall(no(_)),
          ready -> guessBeer,
                   end;
                   fail.
 
 
+:- dynamic yes/1, no/1.
+
+
 /* gather facts from user input */
-:- dynamic fact/1.
 is(Attribute) :-
-  ( fact(Attribute) ->
-    true;
-      format("Would you like your beer to be ~w?~n", [Attribute]),
-      Answer = read(yes),
-      ( Answer ->
-          assert(fact(Attribute))
-      )
+  ((yes(Attribute) ->
+    true, !;
+    no(Attribute) ->
+    false, !;
+    format("Would you like your beer to be ~w?~n", [Attribute]),
+    read(Answer),
+    (Answer = yes ->
+      assert(yes(Attribute)), true, !;
+    Answer = no ->
+      assert(no(Attribute)), false, !;
+    write("Please answer 'yes.' or 'no.'"), nl,
+    is(Attribute)))
   ).
 
 
@@ -26,93 +34,93 @@ is(Attribute) :-
 % 1
 beer(pilsner) :-     is("light-colored"),
                      is("mildly hoppy"),
-                     is("moderately malty"),
-                     is("low in alcohol content"),
+                     is("somewhat malty"),
+                     is("low in alcohol"),
                      nl.
 % 2
 beer(amberLager) :-  is("amber-colored"),
                      is("moderately hoppy"),
-                     is("moderately malty"),
-                     is("moderate in alcohol content"),
+                     is("somewhat malty"),
+                     is("moderate in alcohol"),
                      nl.
 % 3
 beer(paleAle) :-     is("amber-colored"),
                      is("very hoppy"),
-                     is("moderately malty"),
-                     is("high in alcohol content"),
+                     is("somewhat malty"),
+                     is("high in alcohol"),
                      nl.
 % 4
 beer(porter) :-      is("dark-colored"),
                      is("very hoppy"),
                      is("quite malty"),
-                     is("moderate in alcohol content"),
+                     is("moderate in alcohol"),
                      nl.
 % 5
 beer(irishStout) :-  is("dark-colored"),
                      is("moderately hoppy"),
-                     is("moderately malty"),
-                     is("moderate in alcohol content"),
+                     is("somewhat malty"),
+                     is("moderate in alcohol"),
                      nl.
 % 6
 beer(milkStout) :-   is("dark-colored"),
                      is("mildly hoppy"),
                      is("quite malty"),
-                     is("moderate in alcohol content"),
+                     is("moderate in alcohol"),
                      nl.
 % 7
 beer(bock) :-        is("dark-colored"),
                      is("mildly hoppy"),
                      is("quite malty"),
-                     is("high in alcohol content"),
+                     is("high in alcohol"),
                      nl.
 % 8
 beer(wheatBeer) :-   is("amber-colored"),
                      is("mildly hoppy"),
-                     is("moderately malty"),
-                     is("low in alcohol content"),
+                     is("somewhat malty"),
+                     is("low in alcohol"),
                      nl.
 % 9
 beer(brownAle) :-    is("dark-colored"),
                      is("mildly hoppy"),
                      is("quite malty"),
-                     is("moderate in alcohol content"),
+                     is("moderate in alcohol"),
                      nl.
 % 10
 beer(sourBeer) :-    is("light-colored"),
                      is("moderately hoppy"),
-                     is("moderately malty"),
+                     is("somewhat malty"),
                      is("sour"),
-                     is("high in alcohol content"),
+                     is("high in alcohol"),
                      nl.
 % 11
 beer(ipa) :-         is("light-colored"),
                      is("very hoppy"),
-                     is("moderately malty"),
-                     is("high in alcohol content"),
+                     is("somewhat malty"),
+                     is("high in alcohol"),
                      nl.
 % 12
 beer(scottishAle) :- is("amber-colored"),
                      is("mildly hoppy"),
                      is("quite malty"),
-                     is("high in alcohol content"),
+                     is("high in alcohol"),
                      nl.
 % 13
 beer(redAle) :-      is("amber-colored"),
                      is("moderately hoppy"),
-                     is("moderately malty"),
-                     is("high in alcohol content"),
+                     is("somewhat malty"),
+                     is("high in alcohol"),
                      nl.
 % 14
 beer(dubbel) :-      is("dark-colored"),
                      is("moderately hoppy"),
                      is("not too sweet"),
-                     is("moderate in alcohol content"),
+                     is("moderate in alcohol"),
                      nl.
 % 15
 beer(belgianGold) :- is("light-colored"),
                      is("very hoppy"),
                      is("not too sweet"),
-                     is("high in alcohol content"),
+                     is("high in alcohol"),
                      nl.
 
 
